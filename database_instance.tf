13299b2e3a742d478273a27c96e65e2bcea9fa87
LikithaRDahiya <158450437+LikithaRDahiya@users.noreply.github.com>
2024-04-13 18:51:47 +0530

    Create database_instance.tf

---
+++ database_instance.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "dbInstance" {
+  ami                    = "ami-0451f2687182e0411"  # Use the AMI ID for your database instance
+  instance_type          = "t2.micro"
+  key_name               = "docker"
+ count = "1"
+  subnet_id              = aws_subnet.private_subnet.id
+ tags = {
+  Name = "dbInstance"
+ }
+}
