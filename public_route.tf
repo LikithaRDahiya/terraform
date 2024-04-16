ecb69db12bbdd31b82f0f95cf54c86b0d6f554ce
LikithaRDahiya <158450437+LikithaRDahiya@users.noreply.github.com>
2024-04-16 19:47:09 +0530

    Update public_route.tf

--- public_route.tf
+++ public_route.tf
@@ -1,6 +1,6 @@
 resource "aws_route_table" "public_route_table" {
  vpc_id = aws_vpc.my_vpc.id
-
+route_table_id         = aws_vpc.my_vpc.main_route_table_id
  route {
   cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
